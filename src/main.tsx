9835558a72496a23f932ae94ca924e084484adae
matthiathan <84130588+matthiathan@users.noreply.github.com>
2026-07-15 07:48:20 +0200

    Load retro theme on redesign branch

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './styles.css';
 import './live.css';
 import './operations.css';
+import './retro-theme.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><App /></React.StrictMode>,
